ac0227547637014d3cc1a74c5eaae0f08ce2bb2b
imLiaMxo <liammaundrill2015@gmail.com>
2022-03-10 15:23:00 +0000

    added breadcrumbs to forums

--- resources/views/forums/board/show.blade.php
+++ resources/views/forums/board/show.blade.php
@@ -10,8 +10,31 @@
             <div class="text-center">
                 <h2 class="text-3xl tracking-tight font-extrabold text-black dark:text-white sm:text-4xl">{{ $board->name }}</h2>
             </div>
-            <div class="grid grid-cols-5 gap-3">
-                <div class="bg-blue-10 col-span-4">
+                    <!-- Breadcrumbs -->
+                    <div class="dark:bg-gray-700 transition-colors transition p-4 mt-3 mb-3 flex items-center flex-wrap rounded shadow-xl text-black dark:text-white">
+                        <ul class="flex items-center">
+                            <li class="inline-flex items-center">
+                                <a href="{{ route('forums.index') }}">
+                                    <svg class="w-5 h-auto fill-current mx-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1z"/></svg>
+                                </a>
+                                <svg class="w-5 h-auto fill-current mx-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z"/></svg>
+                            </li>
+
+                            <li class="inline-flex items-center">
+                                <a href="{{ route('forums.index') }}">
+                                    Forums
+                                </a>
+                                <svg class="w-5 h-auto fill-current mx-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z"/></svg>
+                            </li>
+                            <li class="inline-flex items-center">
+                                <a href="#">
+                                    {{ $board->name }}
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- End -->
+
                     <!-- Content -->
                     <div class="bg-white dark:bg-gray-700 dark:text-white w-full rounded shadow-xl overflow-hidden p-1 mt-5 transition-500">
                         @forelse($threads as $thread)
@@ -47,11 +70,7 @@
                         {{ $threads->links('pagination::tailwind') }}
                     </div>
                 </div>
-                <div class="col-0">
-                    <!-- Sidebar -->
-                </div>
             </div>
-        </div>
     </div>
 </section>
 @endsection
